faedc8adc90d0f8d5e06ef7055686618d1a7f1ff
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-01 14:34:06 +0200

    Add list boilerplate

--- src/main/java/com/mischiefsmp/perms/commands/CommandPerms.java
+++ src/main/java/com/mischiefsmp/perms/commands/CommandPerms.java
@@ -27,6 +27,17 @@ public class CommandPerms implements CommandExecutor {
         if(args[0].equals("help") && isAllowed(sender, ReadOnly.getCMDPerm("perms.help"))) {
             sendHelp(sender);
             return true;
+        } else if(args[0].equals("list")) {
+            if(args.length < 2) {
+                sendWU(sender);
+                return true;
+            }
+            switch (args[1]) {
+                case "groups" -> listGroups(sender);
+                case "users" -> listUsers(sender);
+                default -> sendWU(sender);
+            }
+            return true;
         } else if(args[0].equals("group")) {
 
         } else if(args[0].equals("user")) {
@@ -42,6 +53,19 @@ public class CommandPerms implements CommandExecutor {
         if(!should)
             sender.sendMessage(LangManager.getString(sender, "noperm"));
         return should;
+    }
+
+    private void listUsers(CommandSender sender) {
+        sender.sendMessage("All users:");
+    }
+
+    private void listGroups(CommandSender sender) {
+        sender.sendMessage("All groups:");
+    }
+
+    private void sendWU(CommandSender sender) {
+        sender.sendMessage(LangManager.getString(sender, "wrong-usage"));
+        sender.sendMessage(LangManager.getString(sender, "try-cmd", ReadOnly.getCMDUsage("perms.help")));
     }
 
     private void sendHelp(CommandSender sender) {
