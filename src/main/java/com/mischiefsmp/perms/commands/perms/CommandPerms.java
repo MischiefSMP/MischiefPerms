8549b359925a3e32d8aec78957febe430fd05228
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-02 21:03:12 +0200

    Reformat code

--- src/main/java/com/mischiefsmp/perms/commands/perms/CommandPerms.java
+++ src/main/java/com/mischiefsmp/perms/commands/perms/CommandPerms.java
@@ -27,9 +27,9 @@ public class CommandPerms implements CommandExecutor {
                 return true;
             }
             switch (args[1]) {
-                case "groups" -> CommandPermsGroup.listGroups(sender);
-                case "users" -> CommandPermsUser.listUsers(sender);
-                default -> CommandPermsUtils.sendWU(sender);
+                case "groups"   -> CommandPermsGroup.listGroups(sender);
+                case "users"    -> CommandPermsUser.listUsers(sender);
+                default         -> CommandPermsUtils.sendWU(sender);
             }
             return true;
         } else if(args[0].equals("group")) {
@@ -40,10 +40,10 @@ public class CommandPerms implements CommandExecutor {
 
             // -> /perms group  one  two   three  four
             // -> /perms group grant <id> <perm> [world]
-            final String one = CommandPermsUtils.arg(args, 1);
-            final String two = CommandPermsUtils.arg(args, 2);
-            final String three = CommandPermsUtils.arg(args, 3);
-            final String four = CommandPermsUtils.arg(args, 4);
+            final String one    = CommandPermsUtils.arg(args, 1);
+            final String two    = CommandPermsUtils.arg(args, 2);
+            final String three  = CommandPermsUtils.arg(args, 3);
+            final String four   = CommandPermsUtils.arg(args, 4);
 
             if(one == null) {
                 CommandPermsUtils.sendWU(sender);
@@ -52,19 +52,18 @@ public class CommandPerms implements CommandExecutor {
 
             //TODO: Split args properly to allow the usage of ""
             switch (one) {
-                case "create" -> CommandPermsGroup.createGroup(sender, two);
-                case "delete" -> CommandPermsGroup.deleteGroup(sender, two);
-                case "clear" -> CommandPermsGroup.clearGroup(sender, two);
-                case "add" -> CommandPermsGroup.addGroup(sender, two, three, four);
-                case "remove" -> CommandPermsGroup.removeGroup(sender, two, three, four);
-                case "prefix" -> CommandPermsGroup.prefixGroup(sender, two, three);
-                case "suffix" -> CommandPermsGroup.suffixGroup(sender, two, three);
-                // -> /perms group admin
-                default -> CommandPermsGroup.infoGroup(sender, one);
+                case "create"   -> CommandPermsGroup.createGroup(sender, two);
+                case "delete"   -> CommandPermsGroup.deleteGroup(sender, two);
+                case "clear"    -> CommandPermsGroup.clearGroup(sender, two);
+                case "add"      -> CommandPermsGroup.addGroup(sender, two, three, four);
+                case "remove"   -> CommandPermsGroup.removeGroup(sender, two, three, four);
+                case "prefix"   -> CommandPermsGroup.prefixGroup(sender, two, three);
+                case "suffix"   -> CommandPermsGroup.suffixGroup(sender, two, three);
+                default         -> CommandPermsGroup.infoGroup(sender, one);
             }
 
         } else if(args[0].equals("user")) {
-            
+
         }
 
         return true;
