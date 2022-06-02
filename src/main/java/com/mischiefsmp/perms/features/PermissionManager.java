c363372955d83a8877d991dd05447bd3a973ed04
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-02 16:34:31 +0200

    Modify PermissionManager to hold groups & users

--- src/main/java/com/mischiefsmp/perms/features/PermissionManager.java
+++ src/main/java/com/mischiefsmp/perms/features/PermissionManager.java
@@ -1,11 +1,24 @@
 package com.mischiefsmp.perms.features;
 
 import com.mischiefsmp.perms.MischiefPerms;
+import com.mischiefsmp.perms.permission.MischiefGroup;
+import com.mischiefsmp.perms.permission.MischiefUser;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class PermissionManager {
     private static MischiefPerms plugin;
+    private static HashMap<String, MischiefGroup> groups = new HashMap<>();
+    private static HashMap<UUID, MischiefUser> users = new HashMap<>();
 
     public static void init(MischiefPerms plugin) {
         PermissionManager.plugin = plugin;
     }
+
+    public static MischiefGroup getGroup(String id) {
+        return groups.get(id);
+    }
+
+
 }
