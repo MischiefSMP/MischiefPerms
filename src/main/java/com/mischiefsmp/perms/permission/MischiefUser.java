0e52f80965daf3fc0175e420e63525dcc8396200
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-02 16:34:18 +0200

    Create MischiefUser.java

---
+++ src/main/java/com/mischiefsmp/perms/permission/MischiefUser.java
@@ -0,0 +1,100 @@
+package com.mischiefsmp.perms.permission;
+
+import com.mischiefsmp.perms.MischiefPerms;
+import com.mischiefsmp.perms.features.PermissionManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class MischiefUser {
+    private UUID uuid;
+    private HashMap<String, MischiefPermission> permissions = new HashMap<>();
+    //private ArrayList<MischiefPermission> permissions;
+    private ArrayList<String> groups = new ArrayList<>(); //We keep a reference to the group's id, not the class
+    private String prefix;
+    private String suffix;
+
+    public boolean canRun(String permission) {
+        return canRun(new MischiefPermission(permission));
+    }
+
+    public boolean canRun(MischiefPermission permission) {
+        if(!permission.isAllowed())
+            MischiefPerms.log("canRun check for permission <%s>. It is denied with the - char. Should this be asked?", Level.WARNING, permission);
+
+        boolean canRun = false;
+        //Check groups
+        for(String groupID : groups) {
+            MischiefGroup g = PermissionManager.getGroup(groupID);
+            if(g != null) {
+                for (MischiefPermission p : g.getPermissions()) {
+                    if(p.equals(permission, true)) {
+                        if(!p.isAllowed()) {
+                            canRun = false;
+                            break;
+                        }
+                    }
+                    if (p.equals(permission)) {
+                        canRun = true;
+                    }
+                }
+            } else {
+                MischiefPerms.log("Group <%s> in MischiefUser <%s> is null! This should not happen!", Level.WARNING, groupID, uuid);
+            }
+        }
+
+        //Check user permission
+        for(String pKey : permissions.keySet()) {
+            MischiefPermission p = permissions.get(pKey);
+            if(p.equals(permission, true)) {
+                if(!p.isAllowed()) {
+                    canRun = false;
+                    break;
+                }
+            }
+            if(p.equals(permission))
+                canRun = true;
+        }
+
+        return canRun;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void addPermission(String permission) {
+        MischiefPermission newPermission = new MischiefPermission(permission);
+        permissions.put(newPermission.toString(), newPermission);
+    }
+
+    public void removePermission(String permission) {
+        permissions.remove(new MischiefPermission(permission).toString());
+    }
+
+    public ArrayList<String> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(ArrayList<String> groups) {
+        this.groups = groups;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+}
